59eb3a9864816917cdfa37587eabe5d9f0460b46
rohit5126 <rohit5126kumar@gmail.com>
2026-02-10 20:56:25 +0530

    Fix syntax errors in pushDocker.groovy

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -5,3 +5,5 @@ def call(String credID){
     usernameVariable:"dockerhubUser")]) {
   sh "docker login -u ${env.dockerhubUser} -p ${env.dockerhubPass}"
   sh "docker push rohit5126/django_app:latest"
+  }
+}
