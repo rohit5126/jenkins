cd19b1d0da7b3910b78d52a6b39a067821a46e97
rohit5126 <rohit5126kumar@gmail.com>
2026-02-10 20:54:39 +0530

    Add pushDocker function for Docker login and push

---
+++ vars/pushDocker.groovy
@@ -0,0 +1,7 @@
+def call(String credID){
+  withCredentials([usernamePassword(
+    credentialsId:"${credID}",
+    passwordVariable:"dockerhubPass",
+    usernameVariable:"dockerhubUser")]) {
+  sh "docker login -u ${env.dockerhubUser} -p ${env.dockerhubPass}"
+  sh "docker push rohit5126/django_app:latest"
